0fac7aa2dcc8d216e09629b7b33296c4f8d96de4
maor828 <bzmaor@gmail.com>
2021-09-02 13:52:28 +0300

    now there are 4 iterations of presentation

--- jpeg_proj.py
+++ jpeg_proj.py
@@ -40,7 +40,7 @@ def main(f,N):
     min_diff = [-1]*4
     i_diff = [0]*4
     j_diff = [0]*4
-    diffs = [np.zeros(np.shape(f))]*4
+    diffs = np.array([np.zeros(np.shape(f))]*4)
     for k in range(4):
         for i in range(N):
             for j in range(N):
@@ -52,12 +52,10 @@ def main(f,N):
                     i_diff[k] = i
                     j_diff[k] = j
                     min_diff[k] = curr_diff
-    first = 0
-    second = 0
-    third = 0
-    return (f_arr_jpeg[first][first,first] , f_arr_jpeg[second][i_diff[second], j_diff[second]], f_arr_jpeg[third][i_comp[third], j_comp[third]],
+    return f_arr_jpeg, diffs, comp_rates, i_diff, j_diff, i_comp, j_comp
+    '''return (f_arr_jpeg[first][first,first] , f_arr_jpeg[second][i_diff[second], j_diff[second]], f_arr_jpeg[third][i_comp[third], j_comp[third]],
             diffs[first][first,first], diffs[second][i_diff[second], j_diff[second]], diffs[third][i_comp[third], j_comp[third]],
-            comp_rates[first][first,first], comp_rates[second][i_diff[second], j_diff[second]], comp_rates[third][i_comp[third], j_comp[third]])
+            comp_rates[first][first,first], comp_rates[second][i_diff[second], j_diff[second]], comp_rates[third][i_comp[third], j_comp[third]])'''
 def diff(mat):
     arr = []
     for row in mat:
@@ -67,52 +65,57 @@ def diff(mat):
 f = cv2.imread("pexels-8296783.jpg", 0)
 print(f.shape)
 #f = f[f.shape[0]//2 - 39:f.shape[0]//2 + 40, f.shape[1]//2 - 39:f.shape[1]//2 + 40] #f[:80,:80]#[:floor(f.shape[0]/8)*8, :floor(f.shape[1]/8)*8]
-f = f[:(f.shape[0]//8)*8, :(f.shape[1]//8)*8]
+#f = f[:(f.shape[0]//8)*8, :(f.shape[1]//8)*8]
+f = f[:80,:80]
 f_prev = np.array(f)
 f_diff = np.array(f)
 f_comp = np.array(f)
-prev_diff = []
-prev_comp = []
-diff_diff = []
-diff_comp = []
-comp_diff = []
-comp_comp = []
+prev_diff = [[]]*4
+prev_comp = [[]]*4
+diff_diff = [[]]*4
+diff_comp = [[]]*4
+comp_diff = [[]]*4
+comp_comp = [[]]*4
+#print(prev_diff)
+
+f_arr_jpeg, diffs, comp_rates, i_diff, j_diff, i_comp, j_comp = (0,0,0,0,0,0,0)
+
+first = 0
+second = 0
+third = 0
+
 for i in range(int(f.shape[0]/8)):
     print(i)
     for j in range(int(f.shape[1]/8)):
         #print(j)
         temp = f[8*i:8*(i+1),8*j:8*(j+1)]
-        (f_prev[8*i:8*(i+1),8*j:8*(j+1)],
-         f_diff[8*i:8*(i+1),8*j:8*(j+1)],
-         f_comp[8*i:8*(i+1),8*j:8*(j+1)],
-         t_prev_diff,
-         t_diff_diff,
-         t_comp_diff,
-         t_prev_comp,
-         t_diff_comp,
-         t_comp_comp)= main(temp,8)
-        prev_diff.append(t_prev_diff)
-        diff_diff.append(t_diff_diff)
-        comp_diff.append(t_comp_diff)
-        prev_comp.append(t_prev_comp)
-        diff_comp.append(t_diff_comp)
-        comp_comp.append(t_comp_comp)
-prev_diff = round(sum(prev_diff)/len(prev_diff), 2)
-prev_comp = round(sum(prev_comp)/len(prev_comp), 2)
-diff_diff = round(sum(diff_diff)/len(diff_diff), 2)
-diff_comp = round(sum(diff_comp)/len(diff_comp), 2)
-comp_diff = round(sum(comp_diff)/len(comp_diff), 2)
-comp_comp = round(sum(comp_comp)/len(comp_comp), 2)
-fig, axs = plt.subplots(1,4)
-axs[0].imshow(f, cmap = 'gray', vmin = 0, vmax = 255)
-axs[0].set_title('original')
-axs[1].imshow(f_prev, cmap = 'gray', vmin = 0, vmax = 255)
-axs[1].set_title('original jpeg\ndistance: ' + str(prev_diff)
-                   + '\ncomp_rate: ' + str(prev_comp))
-axs[2].imshow(f_diff, cmap = 'gray', vmin = 0, vmax = 255)
-axs[2].set_title('best distance\ndistance: ' + str(diff_diff)
-                   + '\ncomp_rate: ' + str(diff_comp))
-axs[3].imshow(f_comp, cmap = 'gray', vmin = 0, vmax = 255)
-axs[3].set_title('best compression\ndistance: ' + str(comp_diff)
-                   + '\ncomp_rate: ' + str(comp_comp))
-plt.show()
+        
+        (f_arr_jpeg, diffs, comp_rates, i_diff, j_diff, i_comp, j_comp)= main(temp,8)
+        for k in range(4):  
+            prev_diff[k].append(diffs[0,0,0])
+            diff_diff[k].append(diffs[k,i_diff[k],j_diff[k]])
+            comp_diff[k].append(diffs[k, i_comp[k],j_comp[k]])
+            prev_comp[k].append(comp_rates[0,0,0])
+            diff_comp[k].append(comp_rates[k,i_diff[k],j_diff[k]])
+            comp_comp[k].append(comp_rates[k, i_comp[k],j_comp[k]])
+for k in range(4):
+    prev_diff[k] = round(sum(prev_diff[k])/len(prev_diff[k]), 2)
+    prev_comp[k] = round(sum(prev_comp[k])/len(prev_comp[k]), 2)
+    diff_diff[k] = round(sum(diff_diff[k])/len(diff_diff[k]), 2)
+    diff_comp[k] = round(sum(diff_comp[k])/len(diff_comp[k]), 2)
+    comp_diff[k] = round(sum(comp_diff[k])/len(comp_diff[k]), 2)
+    comp_comp[k] = round(sum(comp_comp[k])/len(comp_comp[k]), 2)
+    
+    fig, axs = plt.subplots(1,4)
+    axs[0].imshow(f, cmap = 'gray', vmin = 0, vmax = 255)
+    axs[0].set_title('original')
+    axs[1].imshow(f_arr_jpeg[0,0,0], cmap = 'gray', vmin = 0, vmax = 255)
+    axs[1].set_title('original jpeg\ndistance: ' + str(prev_diff[k])
+                       + '\ncomp_rate: ' + str(prev_comp[k]))
+    axs[2].imshow(f_arr_jpeg[k,i_diff[k],j_diff[k]], cmap = 'gray', vmin = 0, vmax = 255)
+    axs[2].set_title('best distance\ndistance: ' + str(diff_diff[k])
+                       + '\ncomp_rate: ' + str(diff_comp[k]))
+    axs[3].imshow(f_arr_jpeg[k,i_comp[k],j_comp[k]], cmap = 'gray', vmin = 0, vmax = 255)
+    axs[3].set_title('best compression\ndistance: ' + str(comp_diff[k])
+                       + '\ncomp_rate: ' + str(comp_comp[k]))
+    plt.show()
